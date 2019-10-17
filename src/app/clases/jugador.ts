56b0b4c1f22208168de27133f3ad9480ae99e846
patoattie <patoattie@gmail.com>
2019-10-17 19:38:23 -0300

    peleandome con firebase

--- src/app/clases/jugador.ts
+++ src/app/clases/jugador.ts
@@ -5,13 +5,15 @@ export class Jugador
 	public idCollection: string;
 	public usuario: string;
     public cuit: number;
-    public sexo: ESexo;
+	public sexo: ESexo;
+	public uid: string;
 
-	constructor(usuario?: string, cuit?: number, sexo?: ESexo, idCollection?: string)
+	constructor(usuario?: string, cuit?: number, sexo?: ESexo, idCollection?: string, uid?: string)
 	{
         this.usuario = usuario;
         this.cuit = cuit;
 		this.sexo = sexo;
 		this.idCollection = idCollection;
+		this.uid = uid;
 	}
 }
